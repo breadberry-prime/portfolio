b1ea4b9a49197011cea8581abe0a874b82647287
Yaron D. Fanger <yaron.fanger@ylabs.ch>
2025-03-13 21:11:05 +0100

    some first chagnes

--- app/data.ts
+++ app/data.ts
@@ -29,91 +29,93 @@ type SocialLink = {
 
 export const PROJECTS: Project[] = [
   {
-    name: 'Motion Primitives Pro',
+    name: 'Ylabs Consulting',
     description:
-      'Advanced components and templates to craft beautiful websites.',
-    link: 'https://pro.motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+      'An AI software factory offering tailored AI solutions to small and medium-sized businesses, focusing on efficiency and innovation.',
+    link: 'https://ylabs.ch/',
+    video: 'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
     id: 'project1',
   },
   {
-    name: 'Motion Primitives',
-    description: 'UI kit to make beautiful, animated interfaces.',
-    link: 'https://motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    name: 'Hippocraticum AI',
+    description:
+      'A company dedicated to integrating AI technology with human expertise to enhance patient care in the healthcare sector.',
+    link: 'https://www.hippocraticum.ai/',
+    video: 'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
     id: 'project2',
   },
 ]
 
 export const WORK_EXPERIENCE: WorkExperience[] = [
   {
-    company: 'Reglazed Studio',
-    title: 'CEO',
-    start: '2024',
+    company: 'Hippocraticum AI',
+    title: 'Co-Founder and CEO',
+    start: '2023',
     end: 'Present',
-    link: 'https://ibelick.com',
-    id: 'work1',
-  },
-  {
-    company: 'Freelance',
-    title: 'Design Engineer',
-    start: '2022',
-    end: '2024',
-    link: 'https://ibelick.com',
+    link: 'https://www.hippocraticum.ai/',
     id: 'work2',
   },
   {
-    company: 'Freelance',
-    title: 'Front-end Developer',
-    start: '2017',
+    company: 'Ylabs GmbH',
+    title: 'Co-Founder and Owner',
+    start: '2019',
     end: 'Present',
-    link: 'https://ibelick.com',
+    link: 'https://ylabs.ch/',
+    id: 'work1',
+  },
+  {
+    company: 'Advokatur Fanger',
+    title: 'Technology Solution Consultant',
+    start: '2023',
+    end: 'Present',
+    link: 'https://www.advokatur-fanger.ch/',
+    id: 'work4',
+  },
+  {
+    company: 'Answerme AI',
+    title: 'Founder and CEO',
+    start: '2023',
+    end: '2024',
+    link: 'https://www.answerme.ai/',
     id: 'work3',
+  },
+  {
+    company: 'Swiss Business Protection',
+    title: 'Security Engineer',
+    start: '2021',
+    end: '2023',
+    link: 'https://www.swissbp.ch/',
+    id: 'work5',
+  },
+  {
+    company: 'PEAX AG',
+    title: 'Fullstack Developer Apprentice',
+    start: '2019',
+    end: '2023',
+    link: 'https://www.peax.ch/',
+    id: 'work6',
   },
 ]
 
 export const BLOG_POSTS: BlogPost[] = [
   {
-    title: 'Exploring the Intersection of Design, AI, and Design Engineering',
-    description: 'How AI is changing the way we design',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+    title: 'What an experience at the AI Festival in Milan!',
+    description:
+      'I had the pleasure of meeting forward-thinking startups that are shaping the future of AI in healthcare.',
+    link: 'https://www.linkedin.com/posts/yaron-fanger_what-an-experience-at-the-ai-festival-in-activity-7301240757242208256-5hq4',
     uid: 'blog-1',
-  },
-  {
-    title: 'Why I left my job to start my own company',
-    description:
-      'A deep dive into my decision to leave my job and start my own company',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-2',
-  },
-  {
-    title: 'What I learned from my first year of freelancing',
-    description:
-      'A look back at my first year of freelancing and what I learned',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-3',
   },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    label: 'Github',
-    link: 'https://github.com/ibelick',
-  },
-  {
-    label: 'Twitter',
-    link: 'https://twitter.com/ibelick',
-  },
-  {
     label: 'LinkedIn',
-    link: 'https://www.linkedin.com/in/ibelick',
+    link: 'https://ch.linkedin.com/in/yaron-fanger/en',
   },
   {
-    label: 'Instagram',
-    link: 'https://www.instagram.com/ibelick',
+    label: 'Hugging Face',
+    link: 'https://huggingface.co/breadberry-prime',
   },
 ]
 
-export const EMAIL = 'your@email.com'
+export const EMAIL = 'yaron.fanger@ylabs.ch'
